05db95c8d5b19b923c65692f45871fe766fb35b5
Evan You <yyx990803@gmail.com>
2016-08-26 16:44:59 -0400

    support customization via webpack options + set default objectAssign option

--- index.js
+++ index.js
@@ -2,18 +2,27 @@
 
 var buble = require('buble');
 var path = require('path');
+var assign = require('object-assign');
 
 module.exports = function BubleLoader(source, inputSourceMap) {
-    try {
-        var transformed = buble.transform(source, {
-            transforms: {
-                modules: false
-            }
-        });
-    } catch (e) {
-        this.emitError(e)
-        return
+    var options = {
+        transforms: {
+            modules: false
+        },
+        objectAssign: 'Object.assign'
+    };
+
+    if (this.options.buble) {
+        assign(options, this.options.buble);
     }
+
+    try {
+        var transformed = buble.transform(source, options);
+    } catch (e) {
+        this.emitError(e);
+        return '';
+    }
+
     var resourcePath = this.resourcePath;
 
     transformed.map.file = resourcePath;
